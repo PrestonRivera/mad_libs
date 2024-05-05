fad0b1b05b0db8f82924ddf602d3353413471380
PrestonRivera <prestonrivera211@gmail.com>
2024-05-04 22:58:49 -0500

    added to the story line

--- mad_libs.py
+++ mad_libs.py
@@ -2,14 +2,16 @@ adjective = input("Selcet an adjective:\n")
 nationality = input("Any nationality:\n")
 person = input("Name a person:\n")
 noun = input("Selcet a noun:\n")
-adjective2 = input("Select a secondary adjective, can be the same as the first adjective if you choose:\n")
-noun2 = input("Select a secondary noun, can be the same as the first noun if you choose:\n")
-
+adjective2 = input("Select an adjective:\n")
+noun2 = input("Select a noun:\n")
+adjective3 = input("Select an adjective:\n")
+adjective4 = input("Select an adjective:\n")
+plural_noun = input("Select a plural noun:\n")
 
 
 
 story = f'''
-Pizza was invented by a {adjective}. {nationality} chef named {person}. To make pizza, you need to take a lump of {noun}, and make a thin, round {adjective2} {noun2}. 
+Pizza was invented by a {adjective}. {nationality} chef named {person}. To make pizza, you need to take a lump of {noun}, and make a thin, round {adjective2} {noun2}. Then you cover it with {adjective3} sauce, {adjective4} cheese, and fresh chopped {plural_noun}.
 
 '''
 
