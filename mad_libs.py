66fc1157f5d9096ffe68fc37636c6c4168ffcc6d
PrestonRivera <prestonrivera211@gmail.com>
2024-05-04 23:52:21 -0500

    created function and used dictionary.

--- mad_libs.py
+++ mad_libs.py
@@ -1,17 +1,27 @@
-adjective = input("Selcet an adjective:\n")
-nationality = input("Any nationality:\n")
-person = input("Name a person:\n")
-noun = input("Selcet a noun:\n")
-adjective2 = input("Select an adjective:\n")
-noun2 = input("Select a noun:\n")
-adjective3 = input("Select an adjective:\n")
-adjective4 = input("Select an adjective:\n")
-plural_noun = input("Select a plural noun:\n")
+def user_select():
+    selection = {
+        "adjective": "",
+        "nationality": "",
+        "person": "",
+        "noun": "",
+        "adjective2": "",
+        "noun2": "",
+        "adjective3": "",
+        "adjective4": "",
+        "plural_noun": "",
+    }
+    
+    for k in selection:
+        selection[k] = input(f"Enter a(n) {k}:\n")
 
+    
+    return selection
 
+words = user_select()
 
 story = f'''
-Pizza was invented by a {adjective}. {nationality} chef named {person}. To make pizza, you need to take a lump of {noun}, and make a thin, round {adjective2} {noun2}. Then you cover it with {adjective3} sauce, {adjective4} cheese, and fresh chopped {plural_noun}.
+Pizza was invented by a {words["adjective"]}. {words["nationality"]} chef named {words["person"]}. To make pizza, you need to take a lump of {words["noun"]}, and make a thin, round {words["adjective2"]} {words["noun2"]}. 
+Then you cover it with {words["adjective3"]} sauce, {words["adjective4"]} cheese, and fresh chopped {words["plural_noun"]}.
 
 '''
 
